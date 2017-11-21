338631925ece61413e05fb76756f2c4350566412
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-11-21 13:25:21 +0100

    add menu save

--- app/REST/menu.endpoint.js
+++ app/REST/menu.endpoint.js
@@ -11,5 +11,31 @@ module.exports = {
                 MenuManager.getMenu().then(reply);
             }
         });
+
+        server.route({
+            method: 'POST',
+            path: '/api/menu',
+            handler(request, reply) {
+                MenuManager.addMenu(request.payload).then(reply);
+            }
+        });
+
+        server.route({
+            method: 'POST',
+            path: '/api/menu/image',
+            config: {
+                payload: {
+                    output: 'stream',
+                    allow: 'multipart/form-data',
+                    parse: true
+                }
+            },
+            handler(request, reply) {
+                const data = request.payload;
+                if (data.file) {
+                    MenuManager.saveImage(data.file).then(reply);
+                }
+            }
+        });
     }
 };
